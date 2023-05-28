317d1628cec36186fb5aea14ca61757396fc4c34
Eric Y. Wang <ericwang919@gmail.com>
2023-05-28 16:38:10 -0500

    djutils

--- djutils/__init__.py
+++ djutils/__init__.py
@@ -2,6 +2,7 @@ from .functions import merge
 from .rows import rowmethod, rowproperty
 from .derived import keys, keymethod, keyproperty
 from .context import cache_rowproperty
+from .utils import key_hash
 from .files import Filepath
 from .errors import MissingError, RestrictionError
 from .schemas import Schema
