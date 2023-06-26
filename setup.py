1752125da7b944366ca0eb82413a949c8b19bc17
Eric Y. Wang <ericwang919@gmail.com>
2023-06-26 13:46:30 -0500

    djutils

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     description="Datajoint Utilities",
     author="Eric Y. Wang",
     author_email="eric.wang2@bcm.edu",
-    packages=find_packages(exclude=[]),
-    install_requires=[],
+    packages=find_packages(),
+    install_requires=[
+        "datajoint>=0.12.9,<0.13.0"
+    ],
 )
