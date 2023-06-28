90999211053d8fb3cf241f14619dd6652f19d41f
Eric Y. Wang <ericwang919@gmail.com>
2023-06-28 13:12:41 -0500

    djutils

--- djutils/cache.py
+++ djutils/cache.py
@@ -5,6 +5,8 @@ from .utils import key_hash
 
 
 class Cache(OrderedDict):
+    """Cache"""
+
     def __init__(self, maxsize=None):
         self.maxsize = float("inf") if maxsize is None else int(maxsize)
 
@@ -15,24 +17,11 @@ class Cache(OrderedDict):
         super().__setitem__(key, value)
 
 
-class RowPropertyCache:
-    def __init__(self, *tables, maxsize=None):
-        self.cache = Cache(maxsize)
-        self.tables = tables
-
-        if self.tables:
-            self.selective = True
-            for table in self.tables:
-                if not issubclass(table, (Table, Keys)):
-                    raise TypeError("Cached table must either be a subclass of datajoint.Table or djutils.Keys")
-        else:
-            self.selective = False
+class RowPropertyCache(Cache):
+    """Row Property Cache"""
 
     def get(self, row, method):
         cls = row.__class__
-
-        if self.selective and cls not in self.tables:
-            return method(row)
 
         if issubclass(cls, Table):
             key = key_hash(dict(row.fetch1("KEY"), _class=cls, _method=method))
@@ -42,10 +31,10 @@ class RowPropertyCache:
             raise TypeError("Cached row property only works on subclasses of datajoint.Table or djutils.Keys")
 
         try:
-            ret = self.cache[key]
+            ret = self[key]
         except KeyError:
             ret = method(row)
-            self.cache[key] = ret
+            self[key] = ret
         return ret
 
 
--- djutils/context.py
+++ djutils/context.py
@@ -3,24 +3,19 @@ from . import cache
 
 
 @contextmanager
-def cache_rowproperty(*tables, maxsize=None):
+def cache_rowproperty(maxsize=None):
     """Enables cacheing of row properties
 
     Parameters
     ----------
-    tables : subclass(dj.UserTable) | subclass(djutils.derived.Keys)
-        if no tables are provided, the the row properties of all tables are cached.
-        if tables are provided, then the row properties of provided tables only are cached.
     maxsize : int | none
         maximum number of cache elements
     """
     prev = cache.rowproperty
 
-    if isinstance(prev, cache.RowPropertyCache) and not prev.selective:
-        pass
-    else:
-        cache.rowproperty = cache.RowPropertyCache(*tables, maxsize=maxsize)
-        
+    if prev is None:
+        cache.rowproperty = cache.RowPropertyCache(maxsize=maxsize)
+
     try:
         yield
     finally:
