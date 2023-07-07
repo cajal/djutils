2b5fa831a8d39d283b31aff0598c2d2b5cb5a97d
Eric Y. Wang <ericwang919@gmail.com>
2023-07-06 20:22:20 -0500

    v0.0.1

--- setup.py
+++ setup.py
@@ -4,10 +4,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="djutils",
-    version="0",
+    version="0.0.1",
     description="Datajoint Utilities",
-    author="Eric Y. Wang",
-    author_email="eric.wang2@bcm.edu",
     packages=find_packages(),
     install_requires=[
         "datajoint>=0.12.9,<0.13.0"
